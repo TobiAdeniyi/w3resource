3179e7e1c1f740102c12dd38cc14f05c0067c77c
Tobi Adeniyi <43269117+TobiAdeniyi@users.noreply.github.com>
2020-07-07 14:13:27 +0100

    Add files via upload

---
+++ Exercises/Python Maths/Ecercises/problem33.py
@@ -0,0 +1,23 @@
+"""33. Write a Python program to add, subtract, multiply and division of two complex numbers."""
+
+
+def complex_breakdown(comp_num):
+    re = comp_num.real
+    im = comp_num.imag
+
+def complex_add(a, b):
+    return a + b
+
+def complex_subtract(a, b):
+    return a - b
+
+def complex_multiply(a, b):
+    return a * b
+
+def complex_devide(a, b):
+    return a / b
+
+print(complex_add(2+3j, 2-3j))
+print(complex_subtract(2+3j, 2-3j))
+print(complex_multiply(2+3j, 2-3j))
+print(complex_devide(2+3j, 2-3j))
